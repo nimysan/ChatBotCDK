ce775050acc55f2b5050879d75035aa8cb2a85f0
nimysan <234aini@gmail.com>
2023-09-06 11:22:13 +0800

    run cdk deploy with parameters

---
+++ run_sample.sh
@@ -0,0 +1,7 @@
+cdk deploy --debug -vvv \
+--parameters ec2KeyPair=us-east-1 \
+--parameters databasePassword=Lxd%*1234 \
+--parameters pgAdmin4UserName=sample@sample.com \
+--parameters pgAdmin4Password=SampleAdmin \
+--parameters pgDatabaseName=knowledge \
+--parameters openaiKeyParam=sk-testxxxxx
