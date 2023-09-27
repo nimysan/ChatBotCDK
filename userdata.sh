5bed8e57d745508e8764c806603c8cd3e2e462b0
nimysan <234aini@gmail.com>
2023-09-27 16:39:09 +0800

    可以工作的userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 dnf update -y
 dnf install git postgresql15 pip docker -y
-echo install ok
-usermod -a -G docker ec2-user
 
+#enable pgadmin4
+usermod -a -G docker ec2-user
 systemctl start docker
 systemctl enable docker
-
-## 等待服务启动完成
-#while ! systemctl is-active docker; do
-#    sleep 1
-#done
-#echo "hhhhh"
-
 # start pgAdmin4
 docker run -d -p 62315:80 \
     -e "PGADMIN_DEFAULT_EMAIL=${pgAdmin4UserName}" \
     -e "PGADMIN_DEFAULT_PASSWORD=${pgAdmin4Password}" \
     -d dpage/pgadmin4
 
+#start gradio app
 cd /home/ec2-user
 #deploy ChatBotWebUI
 git clone https://github.com/nimysan/ChatBotWebUI.git
-chown -R ec2-user ChatBotWebUI
+chown -R ec2-user:ec2-user ChatBotWebUI
 cd ChatBotWebUI
 
-#初始化配置
 #初始化配置
 cat << EOF > config.json
 {
@@ -39,9 +32,6 @@ cat << EOF > config.json
   "openai_key": "${openai_key}"
 }
 EOF
-
-pip3 install -r requirements.txt
-nohup python3 webui.py &
+chown ec2-user:ec2-user config.json
+su ec2-user -s deploy.sh
 echo "########### userdata ###########"
-
-tail -f nohup.out
